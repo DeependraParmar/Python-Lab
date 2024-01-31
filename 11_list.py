9c5eb36ba324a93fb9bb991a0597c2c71c45bc41
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-01-31 12:48:27 +0530

    Did assignments

--- 11_list.py
+++ 11_list.py
@@ -1,4 +1,35 @@
 # Lists in python
 
-list1 = [1,2,4,5,"Hello"]
-print(list1)
+# list1 = [1,2,4,5,"Hello", [5,6,7]]
+# list1.append((10,20,40))
+# sorted_list = list1.sort()
+
+# print(list1)
+# print(sorted_list)
+
+#  eval function can be used to take any list and tuple as input
+# list2 = eval(input())
+# print(type (list2))
+
+
+# list3 = [10,8,28,93]
+# list3.sort()
+
+# print(list3)
+
+# n = int(input("Enter the number for table: "))
+# table = []
+# for i in range(1,11):
+#     table.append(i*n)
+
+# print(table[::])
+
+
+print(-9//7)
+print(type(str(float(4))))
+
+print(10+5%3)
+
+print(6/3.3)
+print(6.0//3.3)
+
