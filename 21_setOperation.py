056ddd7ab9b88328aab6ab02c66aeeeb3e5e1444
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-03 12:26:06 +0530

    Set Operation

---
+++ 21_setOperation.py
@@ -0,0 +1,7 @@
+set1 = {1,2,3,4,5,10}
+set2 = {10,20,30,40,50}
+
+print("Union: ", set1 | set2)
+print("Intersection: ", set1 & set2)
+print("Difference: ", set1 - set2)
+print("Symmetric Difference: ", set1 ^ set2)
