728798922cd9c758a813f4b396e155f316c8f130
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-03 12:32:50 +0530

    Placement problem

---
+++ 22_setPlacementProblem.py
@@ -0,0 +1,5 @@
+student = {'Aditya', 'Aarav', 'Ashutosh', 'Deependra', 'Anonymous🎭'}
+placed = {'Aditya', 'Aarav', 'Ashutosh', 'Deependra'}
+
+print("Placed students are: ", student & placed)
+print("Unplaced students are: ", student - placed)
