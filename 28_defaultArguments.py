7d341f75e9b140a691ec3a3a5b97af79119514f7
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-04-24 12:33:46 +0530

    Default arguments

---
+++ 28_defaultArguments.py
@@ -0,0 +1,5 @@
+def greet(name, message="Good Morning"):
+    print(f"Hello {name}, {message}")
+
+greet("Deependra Parmar", message="Aaj 2.40 pe ghar jao")
+greet("Aditya Paliwal", message="Tum 5 baje tak ruko")
